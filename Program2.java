3c85ded5eb2fb48fafd64752f905a5e59c0fc451
On-cloud7 <priyanka.p62003@gmail.com>
2024-06-12 15:09:42 +0530

    files added

---
+++ Program2.java
@@ -0,0 +1,24 @@
+public class Program2 {
+    public static void main(String[] args) {
+        int [] arr = {6,2,3,4,1,9};
+        findMinAndMax(arr);
+    }
+        static void findMinAndMax (int[] arr){
+            if(arr == null || arr.length == 0)
+            return;
+            
+        int min =arr[0];
+        int max = arr[0];
+        for(int i =0;i<arr.length;i++){
+
+            min = Math.min(min, arr[i]);
+            max = Math.max(max, arr[i]);
+            }
+            System.out.println("maximum value :" +max);
+            System.out.println("minimum value :" + min);
+
+        }
+       
+}
+    
+
--- reverseArray.java
+++ reverseArray.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 public class reverseArray{
     public static void main(String[] args) {
-        System.out.println("the  array :" + );
+        System.out.println("the  array :" );
         int []arr = {10,20,30,40,50,60,70,80,90,100};
         int i=0;
         int j =arr.length-1;
