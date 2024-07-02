455cedfee015b0def7a91014d2e93ce7e1bacb57
On-cloud7 <priyanka.p62003@gmail.com>
2024-07-02 20:56:27 +0530

    files added

---
+++ pattern8.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class pattern8 {
+    public static int max(int arr[]){
+        int max = Integer.MIN_VALUE;
+        for(int i =0;i<arr.length;i++){
+            if(arr[i]>max){
+                max = arr[i];
+            }
+        }
+        return max;
+       
+
+    }
+    public static void main(String[] args) {
+        int [] arr = {2,5,1,3,0};
+       System.out.println("maximum number is " + max(arr));
+
+    }
+    
+}
