39df4d573bd2f1fd243f488ef013f1d439e9fefc
On-cloud7 <priyanka.p62003@gmail.com>
2024-10-05 21:31:13 +0530

    files added

---
+++ Array questions/program17.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class program17 {
+    public static int buysell(int prices[]) {
+        int buyprice = Integer.MAX_VALUE;
+        int maxprofit = 0;
+        for (int i = 0; i < prices.length; i++) {
+            if (buyprice < prices[i]) {
+                int profit = prices[i] - buyprice;
+                maxprofit = Math.max(maxprofit, profit);
+
+            } else {
+                buyprice = prices[i];
+            }
+        }
+        return maxprofit;
+    }
+
+    public static void main(String[]args){
+        int prices[] = {7,1,5,3,6,4};
+        System.out.println(buysell(prices));
+    }
+
+}
